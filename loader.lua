f4bb35a2875620bcee51b4e945253b6a0901e3bd
GersonHub-1 <gersoncomta@gmail.com>
2025-03-22 09:05:52 -0300

    update loader.lua

--- loader.lua
+++ loader.lua
@@ -8,5 +8,30 @@ local Window = Rayfield:CreateWindow({
 })
 
 local Tab = Window:CreateTab("Auto Farm", 4483362458) -- Title, Image
-Section:Set("Section Example")
-local Section = Tab:CreateSection("Section Example")
+
+local Toggle = Tab:CreateToggle({
+    Name = "Auto click",
+    CurrentValue = false,
+    Flag = "Toggle1",
+    Callback = function(Value)
+        _G.AutoClick = Value  -- Atualiza o estado do AutoClick com o Toggle
+    end,
+})
+
+Toggle:Set(false)  -- Inicializa o Toggle como falso
+
+spawn(function()
+    while true do
+        if _G.AutoClick then
+            local args = {
+                [1] = {
+                    [1] = "Grind",
+                    [2] = workspace.Server.Mobs.Zaruto:FindFirstChild("C91A7CCB-7E52-4089-B8BE-848402C63B8D")
+                }
+            }
+
+            game:GetService("ReplicatedStorage").Remotes.Server:FireServer(unpack(args))
+        end
+        wait() -- Evita sobrecarregar o jogo
+    end
+end)
