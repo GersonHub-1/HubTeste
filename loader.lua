ea5d96730ab6556eacc7d0ad64475735481f2866
GersonHub-1 <gersoncomta@gmail.com>
2025-03-22 08:42:08 -0300

    update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,13 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
-   Name = "Hallow Hub | Evo Tycoon",
+   Name = "Alucard Hub | Evo Tycoon",
    Icon = 17091459839,
    LoadingTitle = "Alucard Hub",
    LoadingSubtitle = "By Alucard",
    Theme = "Amethyst"
 })
 
+local Tab = Window:CreateTab("Auto Farm", 4483362458) -- Title, Image
+local Tab = Window:CreateTab("Pqp", "rewind")
+local Section = Tab:CreateSection("Section Example")
+Section:Set("Section Example")
