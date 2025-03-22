2efc670d36a17313daa8de92bb6bedcdc2473960
GersonHub-1 <gersoncomta@gmail.com>
2025-03-22 12:11:35 -0300

    update loader.lua

--- loader.lua
+++ loader.lua
@@ -31,9 +31,13 @@ local args = {
     [2] = "Red Orb",
     [3] = "Magma City"
 }
+local args = {
+    [1] = "collectOrb",
+    [2] = "Red Orb",
+    [3] = "City"
+}
 
 game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
-
         end
         wait()
     end
