9c1a93bd8863aef5cca889f03bf9c3f23a489078
GersonHub-1 <gersoncomta@gmail.com>
2025-03-22 11:29:40 -0300

    update loader.lua

--- loader.lua
+++ loader.lua
@@ -10,12 +10,12 @@ local Window = Rayfield:CreateWindow({
 local Tab = Window:CreateTab("Auto Farm", 4483362458) -- Criando o Tab principal
 
 -- Toggle para Auto Click
-local ToggleAutoClick = Tab:CreateToggle({
-    Name = "Auto Click",
+local ToggleAutoFarm = Tab:CreateToggle({
+    Name = "Auto Farm",
     CurrentValue = false,
-    Flag = "AutoClickToggle",
+    Flag = "ToggleAitoFarm",
     Callback = function(Value)
-        _G.AutoClick = Value
+        _G.AutoOrb = Value
     end,
 })
 
@@ -24,41 +24,16 @@ ToggleAutoClick:Set(false)
 spawn(function()
     while true do
         if _G.AutoClick then
-            local args = {
-                [1] = {
-                    [1] = "Grind",
-                    [2] = workspace.Server.Mobs.Zaruto:FindFirstChild("C91A7CCB-7E52-4089-B8BE-848402C63B8D")
-                }
-            }
-            game:GetService("ReplicatedStorage").Remotes.Server:FireServer(unpack(args))
-        end
+local args = {
+    [1] = "collectOrb",
+    [2] = "Red Orb",
+    [3] = "City"
+}
+
+game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+
+          end 
         wait()
     end
 end)
 
--- Toggle para Auto Egg
-local ToggleAutoEgg = Tab:CreateToggle({
-    Name = "Auto Egg",
-    CurrentValue = false,
-    Flag = "AutoEggToggle",
-    Callback = function(Value)
-        _G.AutoEgg = Value
-    end,
-})
-
-ToggleAutoEgg:Set(false)
-
-spawn(function()
-    while true do
-        if _G.AutoEgg then
-
-           local args = {
-                [1] = "rollChampion",
-                     [2] = "two",
-                     [3] = "shinobi world"
-                  }
-          }
-game:GetService("ReplicatedStorage").Shared.events.RemoteEvent:FireServer(unpack(args))
-        wait()
-    end
-end)
