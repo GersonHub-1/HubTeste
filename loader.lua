52be8e459136fbbd55622d43eee21ab8e38bcdc5
GersonHub-1 <gersoncomta@gmail.com>
2025-03-22 10:42:33 -0300

    update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,11 +7,10 @@ local Window = Rayfield:CreateWindow({
    Theme = "Amethyst"
 })
 
--- Criar aba "Auto Farm"
-local Tab = Window:CreateTab("Auto Farm", 4483362458)
+local Tab = Window:CreateTab("Auto Farm", 4483362458) -- Criando o Tab principal
 
 -- Toggle para Auto Click
-local ToggleClick = Tab:CreateToggle({
+local ToggleAutoClick = Tab:CreateToggle({
     Name = "Auto Click",
     CurrentValue = false,
     Flag = "AutoClickToggle",
@@ -20,9 +19,8 @@ local ToggleClick = Tab:CreateToggle({
     end,
 })
 
-ToggleClick:Set(false)  -- Inicializa o Toggle como falso
+ToggleAutoClick:Set(false) 
 
--- Auto Click Loop
 spawn(function()
     while true do
         if _G.AutoClick then
@@ -34,43 +32,34 @@ spawn(function()
             }
             game:GetService("ReplicatedStorage").Remotes.Server:FireServer(unpack(args))
         end
-        wait() -- Evita sobrecarregar o jogo
-    end
-end)
-
--- Criar uma seção no menu
-local Section = Tab:CreateSection("Opções de Auto Farm")
-
--- Toggle para Auto Egg
-local ToggleEgg = Tab:CreateToggle({
-    Name = "Auto Egg",
-    CurrentValue = false,
-    Flag = "AutoEggToggle",
-    Callback = function(Value)
-        _G.AutoEgg = Value 
-    end,
-})
-
-ToggleEgg:Set(false)  -- Inicializa como falso
-
--- Auto Egg Loop
-spawn(function()
-    while true do 
-        if _G.AutoEgg then 
-            local args = {
-                [1] = {
-                    [1] = "TierBuy",
-                    [2] = workspace.Client.Maps.Zaruto.Interact.Tier,
-                    [3] = "E",
-                    [4] = {}
-                }
-            }
-            game:GetService("ReplicatedStorage").Remotes.Server:FireServer(unpack(args))
-        end
         wait()
     end
 end)
 
--- Criar um divisor visual
-local Divider = Tab:CreateDivider()
-Divider:Set(true)
+-- Toggle para Auto Egg
+local ToggleAutoEgg = Tab:CreateToggle({
+    Name = "Auto Egg",
+    CurrentValue = false,
+    Flag = "AutoEggToggle",
+    Callback = function(Value)
+        _G.AutoEgg = Value
+    end,
+})
+
+ToggleAutoEgg:Set(false)
+
+spawn(function()
+    while true do
+        if _G.AutoEgg then
+
+local args = {
+    [1] = "rollChampion",
+    [2] = "two",
+    [3] = "shinobi world"
+}
+
+game:GetService("ReplicatedStorage").Shared.events.RemoteEvent:FireServer(unpack(args))
+
+        wait()
+    end
+end)
