bd04398144ac97f1c86add24cbd70ba4b0085df1
GersonHub-1 <gersoncomta@gmail.com>
2025-03-22 09:31:54 -0300

    update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,19 +7,22 @@ local Window = Rayfield:CreateWindow({
    Theme = "Amethyst"
 })
 
-local Tab = Window:CreateTab("Auto Farm", 4483362458) -- Title, Image
+-- Criar aba "Auto Farm"
+local Tab = Window:CreateTab("Auto Farm", 4483362458)
 
-local Toggle = Tab:CreateToggle({
-    Name = "Auto click",
+-- Toggle para Auto Click
+local ToggleClick = Tab:CreateToggle({
+    Name = "Auto Click",
     CurrentValue = false,
-    Flag = "Toggle",
+    Flag = "AutoClickToggle",
     Callback = function(Value)
-        _G.AutoClick = Value  -- Atualiza o estado do AutoClick com o Toggle
+        _G.AutoClick = Value
     end,
 })
 
-Toggle:Set(false)  -- Inicializa o Toggle como falso
+ToggleClick:Set(false)  -- Inicializa o Toggle como falso
 
+-- Auto Click Loop
 spawn(function()
     while true do
         if _G.AutoClick then
@@ -29,49 +32,47 @@ spawn(function()
                     [2] = workspace.Server.Mobs.Zaruto:FindFirstChild("C91A7CCB-7E52-4089-B8BE-848402C63B8D")
                 }
             }
-
             game:GetService("ReplicatedStorage").Remotes.Server:FireServer(unpack(args))
         end
         wait() -- Evita sobrecarregar o jogo
     end
 end)
 
-local Section = Tab:CreateSection("Section Example")
+-- Criar uma seção no menu
+local Section = Tab:CreateSection("Opções de Auto Farm")
 
-Section:Set("Section Example")
-
-local Toggle = Tab:CreatToggle({
-  Name = "Auto Egg",
-  CurrentValue = false,
-  Flag = "Toggle1",
-  Callback = function(Value)
-    _G.AutoEgg = Value 
+-- Toggle para Auto Egg
+local ToggleEgg = Tab:CreateToggle({
+    Name = "Auto Egg",
+    CurrentValue = false,
+    Flag = "AutoEggToggle",
+    Callback = function(Value)
+        _G.AutoEgg = Value 
     end,
 })
-Toggle:Set(false)
 
-spawn(function)()
-  while true do 
-    if _G.AutoEgg then 
-      local args = {
-    [1] = {
-        [1] = "TierBuy",
-        [2] = workspace.Client.Maps.Zaruto.Interact.Tier,
-        [3] = "E",
-        [4] = {}
-    }
-}
-end
+ToggleEgg:Set(false)  -- Inicializa como falso
 
-game:GetService("ReplicatedStorage").Remotes.Server:FireServer(unpack(args))
-wait()
-  end 
+-- Auto Egg Loop
+spawn(function()
+    while true do 
+        if _G.AutoEgg then 
+            local args = {
+                [1] = {
+                    [1] = "TierBuy",
+                    [2] = workspace.Client.Maps.Zaruto.Interact.Tier,
+                    [3] = "E",
+                    [4] = {}
+                }
+            }
+            game:GetService("ReplicatedStorage").Remotes.Server:FireServer(unpack(args))
+        end
+        wait()
+    end
 end)
 
-
-
-
+-- Criar um divisor visual
 local Divider = Tab:CreateDivider()
+Divider:Set(true)
 
-Divider:Set(false) -- Whether the divider's visibility is to be set to true or false.
 Rayfield:Destroy()
